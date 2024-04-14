7781fd0482fc8e8dfcf04c0f7bb14d09975c5736
suraj sakhare <sakharesuraj10@gmail.com>
2024-04-14 21:28:51 +0530

    introduction

---
+++ Introduction/first.py
@@ -0,0 +1,37 @@
+'''
+print('hello world',23+45*12)
+name='suraj'
+age =23
+price=21.6714555152
+bool=True
+none=None
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(bool))
+print(type(none))
+print(str(12))
+print(int("112"))
+'''
+
+'''
+name=input("enter your name:")
+print(name)
+'''
+
+'''
+num1=int(input("enter first num: "))
+num2=int(input("enter second num: "))
+print("Total: " ,num1+num2)
+'''
+
+
+"""
+num1=float(input("enter value of side: "))
+print("size of squre: " ,(num1*num1))
+"""
+
+
+
+
+
