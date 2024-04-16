dc138424fbda6ffb84cf159b10af1bf3e118d60f
suraj sakhare <sakharesuraj10@gmail.com>
2024-04-16 20:08:48 +0530

    dict.py added

---
+++ Dictionary & Set/dict.py
@@ -0,0 +1,20 @@
+dict={
+    'name':'suraj',
+    "age":21,
+    12.3:31,
+    'obj':{
+        "roll":23,
+         45:23,
+    },
+    'arr':[1,2,3,4]
+}
+s={1:3}
+print(dict['age'])
+print(dict['obj'])
+print(type(dict))
+print(dict.keys())
+print(dict.values())
+print(dict.items())
+print(dict.get('arr'))
+dict.update(s)
+print(dict)
