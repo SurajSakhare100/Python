01eee4a4fd7d75dc9dbc15744a911e9b1f30b0a0
suraj sakhare <sakharesuraj10@gmail.com>
2024-04-18 21:16:45 +0530

    while loop learn

--- Loops/loops.py
+++ Loops/loops.py
@@ -1,52 +1,82 @@
-i=0
-while i<=50:
-  print("pass",i)
-  i+=1
+# i=0
+# while i<=50:
+#   print("pass",i)
+#   i+=1
 
-# h.w 
-# 1. 
+# # h.w 
+# # 1. 
 
-i=50
-while i>=0:
-  print("pass",i)
-  i-=1
+# i=50
+# while i>=0:
+#   print("pass",i)
+#   i-=1
 
-# h.w 
-# 2. 
-i=int(input("enter num: "))
-j=1
-while j<=10:
-  print(i,"*",j,"=",i*j)
-  j+=1
+# # h.w 
+# # 2. 
+# i=int(input("enter num: "))
+# j=1
+# while j<=10:
+#   print(i,"*",j,"=",i*j)
+#   j+=1
 
-# h.w 
-# 3. 
-range={1:2,2:4,34:5,4:0}
-for item in range:
- print(item)
- print(range[item])
-print(type(range))
+# # h.w 
+# # 3. 
+# range={1:2,2:4,34:5,4:0}
+# for item in range:
+#  print(item)
+#  print(range[item])
+# print(type(range))
 
-# h.w 
-# 4. 
+# # h.w 
+# # 4. 
 
-list=[]
-i=1
-while i<=10:
-    m=i**2
-    list.append(m)
-    i+=1
+# list=[]
+# i=1
+# while i<=10:
+#     m=i**2
+#     list.append(m)
+#     i+=1
 
-print(list)
+# print(list)
 
-# h.w 
-# 5.
+# # h.w 
+# # 5.
 
-tuple=(1, 4, 9, 16, 25, 36, 49, 64, 81, 100) 
+# tuple=(1, 4, 9, 16, 25, 36, 49, 64, 81, 100) 
 
-print(type(tuple)) 
+# print(type(tuple)) 
 
-i=0
-while i<len(tuple):
-    print(tuple[i])
-    i+=1
+# i=0
+# while i<len(tuple):
+#     print(tuple[i])
+#     i+=1
+
+# h.w 6
+# i=1
+# while i<=10:
+#   print("pass",i)
+#   if(i==5):
+#       break
+#   i+=1
+
+# h.w 7
+
+# i=1
+# while i<=10:
+#   if(i%2!=0):
+#       i+=1
+#       continue
+#   print(i)
+#   i+=1
+  
+# h.w 8
+  
+# i=1
+# while i<=10:
+#   if(i%2!=0):
+#       i+=1
+#       continue
+#   print(i)
+#   i+=1
+  
+  
