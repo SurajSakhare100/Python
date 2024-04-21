a0e264f4b1566c1ef45ad096cc947af5c38d5ab5
suraj sakhare <sakharesuraj10@gmail.com>
2024-04-21 17:55:29 +0530

    recursion learn

--- Functions/recursion.py
+++ Functions/recursion.py
@@ -29,3 +29,22 @@ number = 12345
 result = sum_of_digits(number)
 print(f"The sum of digits of {number} is: {result}")
 
+def sum_of_integers(n):
+    if n == 1:
+        return 1
+    else:
+        return n + sum_of_integers(n - 1)
+
+number = 5
+result = sum_of_integers(number)
+print(f"The sum of integers from 1 to {number} is: {result}")
+
+def reverse_string(string):
+    if len(string) == 0:
+        return string
+    else:
+        return reverse_string(string[1:]) + string[0]
+
+text = "Hello, world!"
+reversed_text = reverse_string(text)
+print(f"The reversed string of '{text}' is: '{reversed_text}'")
