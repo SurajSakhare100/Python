c425a6e14b460bc4738c0c4f8ef8ed5d41edac28
suraj sakhare <sakharesuraj10@gmail.com>
2024-04-24 20:12:50 +0530

    file.py

---
+++ File/file.py
@@ -0,0 +1,26 @@
+
+# f = open('D:\my personal drive\OneDrive\Desktop\CODE\DSA\Python\Python\File\example.txt', 'r')
+# print(f.read())
+# f.close()
+
+## another way
+
+# with open('example.txt', 'w') as file:
+#     file.write('Hello, this is a test!\n')
+#     file.write('Writing to a file in Python is easy.\n')
+#     file.close()
+
+# with open('example.txt', 'r') as file:
+#     content = file.read()
+#     print(content)
+#     file.close()
+
+# with open('example.txt', 'a') as file:
+#     file.write('Appending additional content.\n')
+#     file.close()
+
+
+# with open('example.txt', 'r') as file:
+#     content = file.read()
+#     file.close()
+#     print(content)
